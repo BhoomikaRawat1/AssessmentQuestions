1c04ba048b7372fe489f2ec908f0856c1cc15cdc
BhoomikaRawat1 <br2227469@gmail.com>
2026-05-28 22:04:11 +0530

    first commit

---
+++ DAY-1/CountVowelsofString.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    char str[1000];
+    printf("Enter a string\n");
+    scanf("%s",&str);
+    int count=0;
+   for(int i=0;i<str[i]!='\0';i++){
+    if(str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' ||
+    str[i]=='U' || str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || 
+    str[i]=='u'){
+        count++;
+    }
+   }
+   printf("vowels in a string %d",count);
+   return 0;
+}
